61445004b324b792b8f1fd853f83549a1da4b095
aditi <aditipandey017@gmail.com>
2025-03-02 17:40:49 +0530

    first commit

---
+++ PhotoAlbum/PhotoAlbum/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+from . import views     #For linking the views.py file to the urls.py file
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home), 
+    path('home/', views.home),     # This is the URL pattern for the home page
+    path('gallery/', views.gallery),        # This is the URL pattern for the gallery page
+    path('add/', views.add),
+    path('upload/', views.upload, name='upload'),
+    path('show_uploaded_image/', views.show_uploaded_image, name='show_uploaded_image'),
+    path('about/', views.about),
+    path('contact/', views.contact),
+    
+      
+]
---
+++ PhotoAlbum/PhotoAlbum/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+
+
+def home(request):
+    # return HttpResponse("Hello world! This is my home page.")
+    return render(request, 'home.html')    # This will render the home.html template
+
+def gallery(request):
+    # return HttpResponse("This is the about page.")
+    return render(request, 'gallery.html')    # This will render the gallery.html template
+
+
+def add(request):
+   
+    return render(request, 'add.html')    # This will render the add.html template
+
+def upload(request):
+   
+    return render(request, 'upload.html') 
+
+def upload(request):
+    if request.method == "POST" and request.FILES.get('image'):
+        image = request.FILES['image']
+        description = request.POST.get('description')
+        category = request.POST.get('category')
+
+        # Save the uploaded file using FileSystemStorage
+        fs = FileSystemStorage()
+        filename = fs.save(image.name, image)
+        image_url = fs.url(filename)  # Get the file URL
+
+        request.session['uploaded_image'] = {
+            'image_url': image_url,
+            'description': description,
+            'category': category
+        }
+
+        return redirect('show_uploaded_image')  # Redirect to the view that shows the uploaded image
+
+    return render(request, 'add.html')
+
+def show_uploaded_image(request):
+    uploaded_image = request.session.get('uploaded_image', None)
+    return render(request, 'upload.html', {'uploaded_image': uploaded_image})
+
+
+def about(request):
+   
+    return render(request, 'about.html') 
+
+def contact(request):
+   
+    return render(request, 'contact.html') 
